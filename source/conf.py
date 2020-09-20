621bb93efbfe63bdbd0c58a19500963784a31c5c
Greg Elin <greg.elin@govready.com>
2020-09-20 07:10:18 -0400

    Remove old draft runbook/Pivotal content

--- source/conf.py
+++ source/conf.py
@@ -29,9 +29,9 @@ author = 'Josh Tauberer, Greg Elin, Peter Kaminski, Ethan Kaminski'
 github_doc_root = 'https://github.com/GovReady/govready-q/tree/master/docs'
 
 # The short X.Y version
-version = 'v0.9.1.38'
+version = 'v0.9.1.38.1'
 # The full version, including alpha/beta/rc tags
-release = 'v0.9.1.38'
+release = 'v0.9.1.38.1'
 
 
 # -- General configuration ---------------------------------------------------
